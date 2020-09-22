49f4e6edff23411e417f1d4dbf1297bc29fb8f86
Carlos <carlosrocha@mecajun.com>
2020-09-21 21:46:14 -0300

    Updates

---
+++ precache-manifest.22149bb60e3f5cc223074c02f9495fd7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f9ac2b742f19bb3fe6beaa54dce861e7",
+    "url": "/blog/index.html"
+  },
+  {
+    "revision": "b500891e35a014dcbf94",
+    "url": "/blog/static/css/main.56aca22d.chunk.css"
+  },
+  {
+    "revision": "d55efab01b14a6dba729",
+    "url": "/blog/static/js/2.394a8071.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/blog/static/js/2.394a8071.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b500891e35a014dcbf94",
+    "url": "/blog/static/js/main.061e26d9.chunk.js"
+  },
+  {
+    "revision": "5c96b9b9025d2d99847a",
+    "url": "/blog/static/js/runtime-main.158e7dc1.js"
+  }
+]);
